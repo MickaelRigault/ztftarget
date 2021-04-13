9cd1309f89d54f7f99ad8e5ce70e56ce67a154b8
MickaelRigault <mick.rigault@gmail.com>
2021-04-13 09:54:12 +0200

    new ztftarget

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ MAINTAINER = "Mickael Rigault"
 MAINTAINER_EMAIL = "m.rigault@ipnl.in2p3.fr"
 URL = "https://github.com/MickaelRigault/ztftarget/"
 LICENSE = "Apache 2.0"
-DOWNLOAD_URL = "https://github.com/MickaelRigault/ztftarget/tarball/0.2"
-VERSION = "0.2.0"
+DOWNLOAD_URL = "https://github.com/MickaelRigault/ztftarget/tarball/0.3"
+VERSION = "0.3.0"
 
 try:
     from setuptools import setup, find_packages
